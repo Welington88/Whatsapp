fc637aa4b307aec07625007085170831b53813f1
welingtonmarquezini <welington_marquezini88@live.com>
2021-04-08 09:39:38 -0300

    adicionar o banco de dados do firebase

--- app/src/main/java/br/com/wmvs/whatsapp/MainActivity.java
+++ app/src/main/java/br/com/wmvs/whatsapp/MainActivity.java
@@ -3,9 +3,12 @@ package br.com.wmvs.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
