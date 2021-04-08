0be1eba0b17bebdbb9e8053ca41b03e9d1130732
Welington Marquezini <welingtonmarquezini@MacBook-Air-de-Welington.local>
2021-04-08 09:27:31 -0300

    start

---
+++ app/src/main/java/br/com/wmvs/whatsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.wmvs.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
